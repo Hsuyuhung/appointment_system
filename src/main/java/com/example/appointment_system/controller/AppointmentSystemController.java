755bf08e5115e12216a6552fcee2382db5d7639e
Hsuyuhung <115125634+Hsuyuhung@users.noreply.github.com>
2022-12-06 14:08:48 +0800

    Update AppointmentSystemController.java

--- src/main/java/com/example/appointment_system/controller/AppointmentSystemController.java
+++ src/main/java/com/example/appointment_system/controller/AppointmentSystemController.java
@@ -11,11 +11,76 @@ import com.example.appointment_system.vo.AppointmentSystemRes;
 
 @RestController
 public class AppointmentSystemController {
+	
+		@PostMapping(value = "api/create_hospital_info")
+	public AppointmentSystemRes createHospitalInfo(@RequestBody AppointmentSystemReq req) {
+
+		if (!StringUtils.hasText(req.getHospitalId())) {
+			return new AppointmentSystemRes(AppointmentSystemRtnCode.HOSPITAL_ID_EMPTY.getMessage());
+		}
+		
+		if (!StringUtils.hasText(req.getDepartment())) {
+			return new AppointmentSystemRes(AppointmentSystemRtnCode.HOSPITAL_DEPARTMENT_EMPTY.getMessage());
+		}
+
+		if (!StringUtils.hasText(req.getHospitalName())) {
+			return new AppointmentSystemRes(AppointmentSystemRtnCode.HOSPITAL_NAME_EMPTY.getMessage());
+		}
+
+		if (!StringUtils.hasText(req.getPhone())) {
+			return new AppointmentSystemRes(AppointmentSystemRtnCode.HOSPITAL_PHONE_EMPTY.getMessage());
+		}
+
+		if (!StringUtils.hasText(req.getCity())) {
+			return new AppointmentSystemRes(AppointmentSystemRtnCode.HOSPITAL_CITY_EMPTY.getMessage());
+		}
+
+		if (!StringUtils.hasText(req.getDistrict())) {
+			return new AppointmentSystemRes(AppointmentSystemRtnCode.HOSPITAL_DISTRICT_EMPTY.getMessage());
+		}
+
+		if (!StringUtils.hasText(req.getAddress())) {
+			return new AppointmentSystemRes(AppointmentSystemRtnCode.HOSPITAL_ADDRESS_EMPTY.getMessage());
+		}	
+		
+		if(!req.getHospitalId().matches("[A-Z]\\d{3}")) {
+			return new AppointmentSystemRes(AppointmentSystemRtnCode.HOSPITAL_ID_WRONG.getMessage());
+		}
+		
+		if(!req.getPhone().matches("\\d{2}-\\d{3}-\\d{4}|\\d{2}-\\d{4}-\\d{4}")) {
+			return new AppointmentSystemRes(AppointmentSystemRtnCode.HOSPITAL_PHONE_WRONG.getMessage());
+		}
+
+		Hospital hospital = appointmentSystemService.createHospitalInfo(req.getHospitalId(), req.getDepartment(), req.getHospitalName(),
+				req.getPhone(), req.getCity(), req.getDistrict(), req.getAddress());
+		if (hospital == null) {
+			return new AppointmentSystemRes(AppointmentSystemRtnCode.HOSPITAL_ID_EXSITED.getMessage());
+		}
+		return new AppointmentSystemRes(AppointmentSystemRtnCode.CREATE_SUCCESSFUL.getMessage(), hospital);
+	}
+	
+	@PostMapping(value = "api/update_hospital_info")
+	public AppointmentSystemRes updateHospitalInfo(@RequestBody AppointmentSystemReq req) {
+		
+		return appointmentSystemService.updateHospitalInfo(req.getHospitalId(), req.getHospitalName(), req.getPhone(), req.getCity(), req.getDistrict(), req.getAddress());
+	}
+	
+	@PostMapping(value = "api/delete_hospital_info")
+	public AppointmentSystemRes deleteHospitalInfo(@RequestBody AppointmentSystemReq req) {
+		
+		return appointmentSystemService.deleteHospitalInfo(req.getHospitalId());
+	}
+	
+	@PostMapping(value = "api/delete_hospital_department")
+	public AppointmentSystemRes deleteHospitalDepartment(@RequestBody AppointmentSystemReq req) {
+		
+		return appointmentSystemService.deleteHospitalDepartment(req.getHospitalId(), req.getDepartment());
+	}
 
 	@Autowired
 	private AppointmentSystemService appointmentSystemService;
 
-	// «Ø¥ß¯f±w¸ê°T
+	// å»ºç«‹ç—…æ‚£è³‡è¨Š
 	@PostMapping(value = "/api/createPatientInfo")
 	public AppointmentSystemRes createPatientInfo(@RequestBody AppointmentSystemReq req) {
 
@@ -25,7 +90,7 @@ public class AppointmentSystemController {
 
 	}
 
-	// §ó§ï¯f±w¸ê°T---> ID .password §P§O.name eMail§ó§ï
+	// æ›´æ”¹ç—…æ‚£è³‡è¨Š---> ID .password åˆ¤åˆ¥.name eMailæ›´æ”¹
 	@PostMapping(value = "/api/updatePatientInfo")
 	public AppointmentSystemRes updatePatientInfo(@RequestBody AppointmentSystemReq req) {
 
@@ -34,14 +99,14 @@ public class AppointmentSystemController {
 
 	}
 
-	// §ó§ï¯f±w¸ê°T---> ID . password§P§O --> password §ó§ï
+	// æ›´æ”¹ç—…æ‚£è³‡è¨Š---> ID . passwordåˆ¤åˆ¥ --> password æ›´æ”¹
 	@PostMapping(value = "/api/updatePatientPassword")
 	public AppointmentSystemRes updatePatientPassword(@RequestBody AppointmentSystemReq req) {
 
 		return appointmentSystemService.updatePatientPassword(req.getId(), req.getPassword(), req.getNewPassword());
 	}
 
-	// §R°£¯f±w¸ê°T---> ID . password§P§O --> §R°£
+	// åˆªé™¤ç—…æ‚£è³‡è¨Š---> ID . passwordåˆ¤åˆ¥ --> åˆªé™¤
 	@PostMapping(value = "/api/deletePatientPassword")
 	public AppointmentSystemRes deletePatientPassword(@RequestBody AppointmentSystemReq req) {
 
@@ -49,7 +114,7 @@ public class AppointmentSystemController {
 
 	}
 
-	// ¬d¸ß¯f±w°ò¥»°T®§(¤£¥]§t´N¶E¬ö¿ı ¸ò ¹w¬ù¬ö¿ı)
+	// æŸ¥è©¢ç—…æ‚£åŸºæœ¬è¨Šæ¯(ä¸åŒ…å«å°±è¨ºç´€éŒ„ è·Ÿ é ç´„ç´€éŒ„)
 	@PostMapping(value = "/api/searchById")
 	public AppointmentSystemRes searchById(@RequestBody AppointmentSystemReq req) {
 		return appointmentSystemService.searchById(req.getId(), req.getPassword());
