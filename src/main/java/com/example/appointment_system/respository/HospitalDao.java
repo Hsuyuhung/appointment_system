5e2e4f2c42b085bf6a479555c3b29f1a7da51cec
Hsuyuhung <115125634+Hsuyuhung@users.noreply.github.com>
2022-12-06 14:15:18 +0800

    Update HospitalDao.java

--- src/main/java/com/example/appointment_system/respository/HospitalDao.java
+++ src/main/java/com/example/appointment_system/respository/HospitalDao.java
@@ -7,5 +7,11 @@ import com.example.appointment_system.entity.Hospital;
 
 @Repository
 public interface HospitalDao extends JpaRepository<Hospital, String> {
+  
+  public List<Hospital> findByHospitalId(String hospitalId);
+
+	public List<Hospital> deleteByHospitalId(String hospitalId);
+
+	public Hospital findByHospitalIdAndDepartment(String hospitalId, String department);
 
 }
